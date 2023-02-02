0e897a92c319143bfd3d53b930a097a7a7dcdf2d
Ilyas G <Ilyas_Ganiyev@g3.ca>
2023-02-02 17:00:58 -0600

    Teacher code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import Card from "./Card.js";
-import UserForm from "./UserForm.js";
-import UsersList from "./UsersList.js";
+
 const INITIAL_USERS = [
   { id: 1, username: "txsmith", age: 31 },
   { id: 2, username: "bxsmith", age: 22 },
@@ -11,12 +9,7 @@ function App() {
   const addNewUserHandler = (newUser) => {
     setUsersList((prevList) => [...prevList, newUser]);
   };
-  return (
-    <div>
-      <UserForm addNewUser={addNewUserHandler} />
-      <UsersList list={usersList} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
